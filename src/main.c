8a94513e99083e96203625a40965cdc173096a3a
Notforest <notforest69@gmail.com>
2023-12-03 18:23:40 +0100

    Used the macro for inline assembly

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 */
 
 void main(void) {
-    __asm__ __volatile__ ("movb $'K', %al\n");
-    __asm__ __volatile__ ("movb $0x0e, %ah\n");
-    __asm__ __volatile__ ("int $0x10\n");
+    asm (
+        "movb $'K', %al\n"
+        "movb $0x0e, %ah\n"
+        "int $0x10\n"
+    );
 }
