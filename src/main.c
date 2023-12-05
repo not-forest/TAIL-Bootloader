7701d5a70b06f39b94d4cbe8cc9fcf3e5d5f72f8
Notforest <notforest69@gmail.com>
2023-12-06 00:52:05 +0100

    Greeting the world for the first time

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 /*
     A main entry of the TAIL bootloader.
 */
+asm(".code16gcc");
+
+#include "memory_map.h"
+#include "bios/video_servises.h"
 
 void main(void) {
-    asm (
-        "movb $'K', %al\n"
-        "movb $0x0e, %ah\n"
-        "int $0x10\n"
-    );
+    print_str("Hello bootloader!");
+
+    while(1) {}
 }
