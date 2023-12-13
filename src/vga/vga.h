93862e4d667a4f45d89c2add344a168a4265057b
Notforest <notforest69@gmail.com>
2023-12-13 16:10:59 +0100

    A better printing functions that provide full control over the buffer when it comes to write characters into it. The printc and prints function are updated to be compatible with the dimensions of the buffer

--- src/vga/vga.h
+++ src/vga/vga.h
@@ -13,7 +13,7 @@
 
 #define BUFFER_PTR 0xb8000
 #define BUFFER_WIDTH 80
-#define BUFFER_HEIGHT 20
+#define BUFFER_HEIGHT 25
 
 #define COLOR_BLACK 0x0
 #define COLOR_BLUE 0x1
@@ -37,29 +37,97 @@ typedef struct {
     uint8_t row, col;
 } VGABuffer;
 
+/* Shifts the values in the buffer by rows 
+
+The function is not suitable for switching rows, but only shift to a
+given location. Values must be within the height of the buffer. */
+static inline void vga_shift(uint8_t old, uint8_t new) {
+    // Pointer to old row buffer.
+    volatile uint16_t* old_ptr = (volatile uint16_t*)BUFFER_PTR + old * BUFFER_WIDTH;
+    // Pointer to new row buffer.
+    volatile uint16_t* new_ptr = old_ptr - (old - new) * BUFFER_WIDTH;
+
+    // Shifts all values in the buffer by each double word.
+    for(uint8_t col_offset = 0; col_offset < BUFFER_WIDTH; col_offset += 1) {
+        *(new_ptr + col_offset) = *(old_ptr + col_offset);
+    }
+}
+
+/* Swaps two rows instead of shifting one to another. 
+
+The swapping is done via XOR operation for each bit*/
+static inline void vga_swap(uint8_t old, uint8_t new) {
+    // Pointer to old row buffer.
+    volatile uint16_t* old_ptr = (volatile uint16_t*)BUFFER_PTR + old * BUFFER_WIDTH;
+    // Pointer to new row buffer.
+    volatile uint16_t* new_ptr = old_ptr - (old - new) * BUFFER_WIDTH;
+
+    /* Shifts all values in the buffer by each double word.
+    The x86 xchg instruction is being used. */
+    for(uint8_t col_offset = 0; col_offset < BUFFER_WIDTH; col_offset += 1) {
+        asm (
+            "xchg %0, %1"
+            : "=r"(*(new_ptr + col_offset))
+            : "m"(*(old_ptr + col_offset)), ""(*(new_ptr + col_offset))
+            : "memory"
+        );
+    }
+}
+
 /* Prints out a single char by writing the data into the buffer.
 
 the background and foreground colors will be provided with the char. If the
 fourth bit of the background color is set, the character will blink like the
 underscore symbol. If the fourth bit of the foreground color is set, the
-character will be bright. */
-void printc(unsigned char c, uint8_t fr, uint8_t bg, VGABuffer* vga) {
-    // Pointer to the VGA buffer.
-    volatile uint16_t* buf = (volatile uint16_t*)BUFFER_PTR + vga->col;
-    uint16_t char_set = (bg << 12) | (fr << 8) | c; // Creating a formatted char for buffer.
+character will be bright.
 
-    *buf = char_set; // Writing to the buffer and changing the cursor's position.
-    ++vga->col;
+It will always return 1 as long as it will not receive a null character.*/
+int printc(unsigned char c, uint8_t fr, uint8_t bg, VGABuffer* vga) {
+    /* Special case for the null character */
+    if (c == '\0') {
+        return 0;
+    }
+    
+    /* Special case for new line characters */
+    if (c != '\n') {
+        // Pointer to the VGA buffer.
+        volatile uint16_t* buf = (volatile uint16_t*)BUFFER_PTR + (vga->row * BUFFER_WIDTH + vga->col);
+        uint16_t char_set = (bg << 12) | (fr << 8) | c; // Creating a formatted char for buffer.
+
+        *buf = char_set; // Writing to the buffer and changing the cursor's position.
+    }
+
+    /* Checking the buffer bounds */
+    if (c == '\n' || ++vga->col >= BUFFER_WIDTH) {
+        vga->col = 0;
+        if (vga->row + 1 >= BUFFER_HEIGHT) {
+            /* Shifting each row up. */
+            for(uint8_t r = 1; r < BUFFER_HEIGHT; ++r) {
+                vga_shift(r, r - 1);
+            }
+        } else {
+            ++vga->row;
+        }
+    }
+
+    return 1;
 }
 
 /* Prints the provided string into the buffer.
 
 The provided atributes are used to every symbol within the string. */
 void prints(char* str, uint8_t fr, uint8_t bg, VGABuffer* vga) {
-    while(*str) {
-        printc(*str, fr, bg, vga);
+    while(printc(*str, fr, bg, vga)) {
         ++str;
     }
 }
+
+/* Works the same as the regular 'prints' except that is adds a newline symbol. */
+static inline void println(char* str, uint8_t fr, uint8_t bg, VGABuffer* vga) {
+    prints(str, fr, bg, vga);
+    vga->col = 0;
+    ++vga->row;
+}
+
 
 #endif
