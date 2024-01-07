c34a0e4e84217ea5dd03dc8f2abc174493bc2d67
Notforest <notforest69@gmail.com>
2024-01-07 14:10:30 +0100

    Testing out the cursor.

--- src/main.c
+++ src/main.c
@@ -9,9 +9,7 @@
 #include<inttypes.h>
 
 #include "memory_map.h"
-
 #include "vga/vga.h"
-#include "vga/presets.h"
 
 // Screen definitions.
 #define LOADER   0b00
@@ -19,10 +17,8 @@
 #define TERMINAL 0b10
 #define INFO     0b11
 
-// Draws the info screen with the logo. 
-void draw_info(uint_fast8_t row);
 // Draws the main loader screen.
-void draw_loader();
+// void draw_loader();
 
 /* A global variable that provide the settings information.
 
@@ -31,26 +27,25 @@ bits are the background color and the next two bits are the current user's scree
 This can be changed via settings screen. */
 static volatile uint16_t CURRENT_SETTINGS = LOADER << 7 | COLOR_BLACK << 4 | COLOR_WHITE;
 
+// The main entry. The boot drive argument is pushed from the HEAD part (mbr boot sector).
 void main(uint16_t boot_drive) {
-    draw_info(1);
+    VGABuffer buf = {.row = 0, .col = 0};
+    enable_cursor(0, 15);
+    
+    println(
+        "Hello cursor.",
+        CURRENT_SETTINGS & COLOR_MASK,
+        (CURRENT_SETTINGS >> 4) & COLOR_MASK,
+        &buf
+    );
 
+    update_cursor(8, 77);
+    
     while(1) {}
 }
 
 
-/* For each row within the info screen the corresponding symbols will be written. */
-void draw_info(uint_fast8_t row) {
-    
-    switch (row)
-    {
-    case 0:
-    case 6:
-    case 7:
-    case 13:
-        break;
-    default:
-        VGABuffer i_vga = {.row = row, .col = 0};
-        
-        prints(INFO_SCREEN_DATA[row], CURRENT_SETTINGS & COLOR_MASK, SETTINGS >> 4 & COLOR_MASK, &i_vga);
-    }
-}
+/* Draws a loader screen. */
+// void draw_loader() {
+
+// }
