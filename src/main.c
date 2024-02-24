653471310b818818a5fc3f28a7f2222f622bd6ce
Notforest <notforest69@gmail.com>
2024-02-24 18:13:22 +0100

    Setting up the IDT and the PIC controller. Checking the work of timer interrupts.

--- src/main.c
+++ src/main.c
@@ -5,11 +5,20 @@
     and load the kernel itself.
 */
 
-// Includes
-#include<inttypes.h>
+// CPU defines
+#define _PIC_               // Using regular PIC instead of modern APIC.
+#define _VGA_INTERFACE_      // Output to regular VGA buffer.
 
-#include "memory_map.h"
-#include "vga/vga.h"
+// Includes
+#include<stdint.h>
+
+#include"memory_map.h"
+
+#include"io/vga.h"
+
+#include"cpu/pic.h"
+#include"cpu/int.h"
+#include"cpu/handlers.h"
 
 // Screen definitions.
 #define LOADER   0b00
@@ -17,35 +26,84 @@
 #define TERMINAL 0b10
 #define INFO     0b11
 
-// Draws the main loader screen.
-// void draw_loader();
+extern HANDLER_FN DIVBZ;
+extern HANDLER_FN PIC_TIMER;
+
+extern void remap_pic(uint8_t master_offset);
+
+void draw_loader();
+void init();
+
+// A global static buffer.
+volatile VGABuffer BUFF = {.row = 0, .col = 0};
 
 /* A global variable that provide the settings information.
-
-The lower three bits decide about the text color, the next three
-bits are the background color and the next two bits are the current user's screen.
-This can be changed via settings screen. */
-static volatile uint16_t CURRENT_SETTINGS = LOADER << 7 | COLOR_BLACK << 4 | COLOR_WHITE;
+ *
+ * The lower four bits decide about the text color, the next four bits are the background
+ * color and the last two bits are the current user's screen. This can be changed via 
+ * settings screen. 
+ *
+ * */
+volatile union {
+    struct {
+        uint8_t fr: 4;
+        uint8_t bg: 4;
+        uint8_t screen: 2; 
+    } fields;
+    uint8_t color_set;
+    uint16_t data;
+} CURRENT_SETTINGS = { .fields.fr = COLOR_WHITE, .fields.bg = COLOR_BLACK, .fields.screen = LOADER };
 
 // The main entry. The boot drive argument is pushed from the HEAD part (mbr boot sector).
 void main(uint16_t boot_drive) {
-    VGABuffer buf = {.row = 0, .col = 0};
-    enable_cursor(0, 15);
-    
-    println(
-        "Hello cursor.",
-        CURRENT_SETTINGS & COLOR_MASK,
-        (CURRENT_SETTINGS >> 4) & COLOR_MASK,
-        &buf
-    );
+    // Initialize
+    init();
+    // TUI
+    draw_loader();  // Drawing the loader screen first.
 
-    update_cursor(8, 77);
-    
-    while(1) {}
+    for(;;) wfi();  // Waits for interrupt
 }
 
+// Initialize the bootloader itself. This does not affect the future OS.
+void init() {
+    // Defining the new IDT.
+    IDT idt;
+    uint16_t cs;
 
-/* Draws a loader screen. */
-// void draw_loader() {
+    // Reading the cs segment.
+    __asm__("mov %%cs, %w0\n":"=a"(cs)::"memory");
 
-// }
+    // Defining the interrupt and exception handlers. 
+    idt.vector_table[0] = (GateDescriptor) { 
+        .offset_0_15 = DIVBZ.bits[0],
+        .selector = cs,
+        .reserved = 0,
+        .attr = TRAP_GATE, 
+        .offset_16_31 = DIVBZ.bits[1],
+    }; 
+    
+    idt.vector_table[32] = (GateDescriptor) { 
+        .offset_0_15 = PIC_TIMER.bits[0],
+        .selector = cs,
+        .reserved = 0,
+        .attr = INT_GATE, 
+        .offset_16_31 = PIC_TIMER.bits[1],
+    };
+
+    // Final initialization functions.
+    lidt(&idt); // Setting the amount of interrupts needed for the bootloader.
+    remap_pic(32); // Mapping the PIC software interrupts to 32th index of the IDT.
+}
+
+// Draws the main menu of the loader.
+void draw_loader() {
+
+}
+
+// Executes when some assrtions fail.
+void __assert_fail (const char *__assertion, const char *__file, unsigned int __line, const char *__function) {
+    prints("Assertion failed: ", CURRENT_SETTINGS.color_set, &BUFF);
+    println(__assertion, CURRENT_SETTINGS.color_set, &BUFF);
+    for(;;);
+};
+
