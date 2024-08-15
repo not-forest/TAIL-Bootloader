5feb1ee27610fa5943a3cab66019ccab4ba471d9
Notforest <notforest69@gmail.com>
2024-08-15 23:58:00 +0200

    Added a global variable for selected OS ID, for further loading.

--- src/insc.h
+++ src/insc.h
@@ -5,6 +5,12 @@
 /* Static global application timer */
 #include <stdint.h>
 
+#define UNSELECTED 0xff
+
+// ID of the selected OS from the OS list. Value '0xff' is considered as not selected yet.
+static volatile uint8_t os_id = UNSELECTED;
+
+// Timer that is globally available across the bootloader. Ticks on every PIC's clock tick.
 static volatile union {
     uint64_t bits;
 } GLOBAL_TIMER;
